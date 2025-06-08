eb34eb10630d11b687265ee175174f4be385076b
vwtorr <vitor.silva29228@alunos.ufersa.edu.br>
2025-06-08 12:38:02 -0300

    cors 2.0

--- src/main.ts
+++ src/main.ts
@@ -8,15 +8,27 @@ import * as express from 'express';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   
-  app.enableCors({
-  origin: [
+    const allowedOrigins = [
     'https://front-projeto-clinica.vercel.app',
     'https://front-projeto-clinica-git-main-vitor-gabriels-projects-82d1d834.vercel.app',
     'https://front-projeto-clinica-psy2gkdkj.vercel.app'
-  ],
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  credentials: true,
-});
+  ];
+  
+  app.enableCors({
+    origin: (origin, callback) => {
+      // O 'origin' será a URL exata do seu front-end.
+      // Em ambientes de teste, requisições do mesmo servidor (ex: Postman) podem não ter 'origin'.
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        console.log('CORS Aprovado para a origem:', origin);
+        callback(null, true);
+      } else {
+        console.log('CORS Bloqueado para a origem:', origin);
+        callback(new Error('Origem não permitida por CORS'));
+      }
+    },
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Clinica')
